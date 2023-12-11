cdaf1fadd2b60707d91a72460dafe779296cdf5f
cuong02n <cuong02n@gmail.com>
2023-12-11 13:09:41 +0700

    init backend

---
+++ food-order-backend/food-order-backend/src/main/java/com/dtvn/foodorderbackend/FoodOrderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dtvn.foodorderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderBackendApplication.class, args);
+    }
+
+}
